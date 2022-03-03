57da41a00cf380006cfc4c3d4ed6e1792e46a9d3
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-03-03 18:11:24 -0500

    fix

--- index.js
+++ index.js
@@ -344,7 +344,7 @@ const run = async (
     select: function(info) {
       location.href='/view/${view_to_create}?${start_field}=' + info.startStr ${end_field ? (`+ '&` + end_field + `=' + info.endStr`) : ""};
     },
-    function(info) {
+    eventDrop: function(info) {
       alert(info.event.title + " was dropped on " + info.event.start.toISOString());
       const old_event = await(Table.findOne({id: context.table_id}).getRow({id: info.event.id})); //look up the event from db
       console.log(old_event.start);
