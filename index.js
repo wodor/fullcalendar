661edb3c7a0a21df3da1254c4374434afc6ed024
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-03-03 19:03:54 -0500

    make draggable, fix id

--- index.js
+++ index.js
@@ -313,6 +313,7 @@ const run = async (
       end,
       url,
       color,
+      id,
     };
   });
   return div(
@@ -343,7 +344,8 @@ const run = async (
     selectable: true,
     select: function(info) {
       location.href='/view/${view_to_create}?${start_field}=' + info.startStr ${end_field ? (`+ '&` + end_field + `=' + info.endStr`) : ""};
-    },` : "" }
+    },
+    eventStartEditable: true,` : "" }
     events: ${JSON.stringify(events)}
   });
   calendar.render();`)
