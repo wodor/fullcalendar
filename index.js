93a4e4b5c5355f3eccec0ee2b19c5501005ee241
kevin8181 <66894759+kevin8181@users.noreply.github.com>
2022-03-03 19:08:08 -0500

    test eventDrop

--- index.js
+++ index.js
@@ -345,7 +345,10 @@ const run = async (
     select: function(info) {
       location.href='/view/${view_to_create}?${start_field}=' + info.startStr ${end_field ? (`+ '&` + end_field + `=' + info.endStr`) : ""};
     },
-    eventStartEditable: true,` : "" }
+    eventStartEditable: true,
+    eventDrop function(info) {
+      alert(info.event.title + " was dropped on " + info.event.start.toISOString());
+    }` : "" }
     events: ${JSON.stringify(events)}
   });
   calendar.render();`)
