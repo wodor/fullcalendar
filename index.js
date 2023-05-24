a3e3dc7b9a624ffcd9acb50a0ecc13c13830fa33
Artur 'Wodor' Wielogorski <wodor@wodor.net>
2023-05-25 01:56:09 +0200

    Avoid overriding configured custom views
    The value of the "Advanced: Custom calendar views" was ignored, this approach allows to not overwrite the `views:` value sent to fullCalendar

--- index.js
+++ index.js
@@ -657,8 +657,7 @@ const run = async (
       right: '${calendar_view_options}',
     },
     navLinks: true,
-    initialView: '${initialView}',
-    ${custom_calendar_views ? custom_calendar_views + "," : ""}
+    initialView: '${initialView}'
     nowIndicator: ${nowIndicator},
     weekNumbers: ${weekNumbers},
     eventColor: '${default_event_color}',
@@ -865,7 +864,9 @@ const run = async (
     views: {
       dayGridWeek: dayGridWeekOpts,
       timeGridWeek: timeGridWeekOpts,
+      ${custom_calendar_views ? custom_calendar_views + "," : ""},
     },
+    
   });
   calendar.render();`)
     ),
